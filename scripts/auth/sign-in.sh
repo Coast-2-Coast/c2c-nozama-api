1174695c6908a3c39ab11798884c230f36a88e09
rlarsonil71 <ron.larson@libertymutual.com>
2017-05-16 15:08:27 -0400

    Added command line tests for user auth CURL scripts
    Successfully tested all 4 user auth CURL scripts in scripts/auth
     - sign-up
     - sign-in
     - change-password
     - sign-out

--- scripts/auth/sign-in.sh
+++ scripts/auth/sign-in.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# $ EMAIL=c2c@wdi.com PASSWORD=triplets scripts/auth/sign-in.sh
+
 API="http://localhost:4741"
 URL_PATH="/sign-in"
 
--- scripts/auth/sign-out.sh
+++ scripts/auth/sign-out.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# $ ID=591b49bee095a321f752b2fe TOKEN=+2QGA4ndDETu0QzNNi86ThW6kwFCCoLoU+LdAWmlcsw=--fIBq4gVGRHBT3H6+2CanB7DjPRjjkDsOOGtDkOBGnQo= scripts/auth/sign-out.sh
+
 API="http://localhost:4741"
 URL_PATH="/sign-out"
 
